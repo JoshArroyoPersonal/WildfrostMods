057caa7f3a8cfa930855b4589bace510db905d46
JoshArroyoUF <joshuaarroyo@ufl.edu>
2024-04-06 19:11:40 -0400

    Put in new pokemon, they still need evos.

---
+++ Pokefrost/ScriptableAmounts.cs
@@ -0,0 +1,41 @@
+﻿using Rewired;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokefrost
+{
+    internal class ScriptableMissingHealth : ScriptableAmount
+    {
+        public override int Get(Entity entity)
+        {
+            if (!entity)
+            {
+                return 0;
+            }
+
+            return entity.hp.max - entity.hp.current;
+        }
+
+    }
+
+    public class TargetConstraintHasSlowkingCrown : TargetConstraint
+    {
+        public override bool Check(Entity target)
+        {
+            return Check(target.data);
+        }
+
+        public override bool Check(CardData targetData)
+        {
+            if (!(targetData.upgrades.Find((CardUpgradeData a) => a.name == "websiteofsites.wildfrost.pokefrost.CrownSlowking") != null))
+            {
+                return not;
+            }
+
+            return !not;
+        }
+    }
+}
---
+++ Pokefrost/StatusEffectApplyXWhileStatused.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using static Rewired.Controller;
+
+namespace Pokefrost
+{
+    internal class StatusEffectApplyXWhileStatused : StatusEffectData
+    {
+
+        [SerializeField]
+        public StatusEffectData effectToGain;
+
+        public int currentAmount;
+
+        public bool active;
+
+        public override bool HasEnableRoutine => true;
+
+        public override bool HasDisableRoutine => true;
+
+        public override bool HasPostHitRoutine => true;
+
+        public override bool HasTurnEndRoutine => true;
+
+        public override bool RunEnableEvent(Entity entity)
+        {
+            return entity == target;
+        }
+
+        public override IEnumerator EnableRoutine(Entity entity)
+        {
+            return Check();
+        }
+
+        public override bool RunDisableEvent(Entity entity)
+        {
+            if (entity == target)
+            {
+                return currentAmount != 0;
+            }
+
+            return false;
+        }
+
+        public override IEnumerator DisableRoutine(Entity entity)
+        {
+            return Deactivate();
+        }
+
+        public override bool RunPostHitEvent(Hit hit)
+        {
+            return hit.target == target;
+        }
+
+        public override IEnumerator PostHitRoutine(Hit hit)
+        {
+            return Check();
+        }
+
+        public override IEnumerator TurnEndRoutine(Entity entity)
+        {
+            return Check();
+        }
+
+        public IEnumerator Check()
+        {
+            if (!target.alive)
+            {
+                yield break;
+            }
+
+            bool flag = false;
+            for (int num = target.statusEffects.Count - 1; num >= 0; num--)
+            {
+                StatusEffectData statusEffectData = target.statusEffects[num];
+                if (statusEffectData.isStatus && statusEffectData.offensive && statusEffectData.visible)
+                {
+                    flag = true;
+                    if (!active)
+                    {
+                        yield return Activate();
+                    }
+                    break;
+                }
+            }
+
+            if (active && !flag)
+            {
+                yield return Deactivate();
+            }
+        }
+
+        public IEnumerator Activate()
+        {
+            currentAmount = GetAmount();
+            yield return StatusEffectSystem.Apply(target, target, effectToGain, currentAmount, temporary: true);
+            active = true;
+        }
+
+        public IEnumerator Deactivate()
+        {
+            for (int num = target.statusEffects.Count - 1; num >= 0; num--)
+            {
+                StatusEffectData statusEffectData = target.statusEffects[num];
+                if ((bool)statusEffectData && statusEffectData.name == effectToGain.name)
+                {
+                    yield return statusEffectData.RemoveStacks(currentAmount, removeTemporary: true);
+                    break;
+                }
+            }
+
+            currentAmount = 0;
+            active = false;
+        }
+
+        public override bool RunEffectBonusChangedEvent()
+        {
+            if (target.enabled && active)
+            {
+                ActionQueue.Add(new ActionSequence(ReAffect()));
+            }
+
+            return false;
+        }
+
+        public IEnumerator ReAffect()
+        {
+            yield return Deactivate();
+            yield return Activate();
+        }
+
+    }
+}
