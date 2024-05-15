6b9f0533cba8bc315fdc842fec33f29478702fab
Michael Coopman <mhcoopman@gmail.com>
2024-05-15 16:32:06 -0400

    Musha finished. Gengar has fading.

--- Pokefrost/StatusEffectChangeData.cs
+++ Pokefrost/StatusEffectChangeData.cs
@@ -4,19 +4,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Pokefrost
 {
     internal class StatusEffectChangeData : StatusEffectData
     {
         public CardData cardBase;
-        public int keepIndex = 0;
+        public Sprite sprite;
+        public int keepHowMany = 5; //temp dream + trigger dreamer + destroy on discard + consume effect + dream morph
 
         public override void Init()
         {
             base.OnTurnEnd += TurnStart;
         }
 
+        public override bool RunBeginEvent()
+        {
+            target.data.backgroundSprite = sprite;
+            target.data.playType = Card.PlayType.Play;
+            return false;
+        }
         private IEnumerator TurnStart(Entity entity)
         {
             if (entity == target)
@@ -25,19 +33,21 @@ namespace Pokefrost
                 yield return UpdateData();
                 target.PromptUpdate();
                 yield return target.display.UpdateData(true);
+                Card card = target.display as Card;
+                card.SetName(cardBase.title);
             }
             yield break;
         }
 
         public IEnumerator RemoveData()
         {
-            for (int i = target.traits.Count - 1; i >= 0; i--)
+            for (int i = target.traits.Count - 1; i >= 1; i--)
             {
                 target.traits[i].count -= target.traits[i].tempCount;
                 target.traits[i].tempCount = 0;
                 yield return target.UpdateTraits(target.traits[i]);
             }
-            for (int i = target.statusEffects.Count - 1; i >= 1; i--)
+            for (int i = target.statusEffects.Count - 1; i >= keepHowMany; i--)
             {
                 StatusEffectData status = target.statusEffects[i];
                 yield return status.Remove();
@@ -46,11 +56,11 @@ namespace Pokefrost
 
         public void ChangeCard()
         {
-            IEnumerable<CardData> cards = AddressableLoader.GetGroup<CardData>("CardData").InRandomOrder();
+            IEnumerable<CardData> cards = InPettyRandomOrder(AddressableLoader.GetGroup<CardData>("CardData"));
             foreach (CardData card in cards)
             {
-                UnityEngine.Debug.Log($"[Test] {card.title}");
-                if (card.cardType.name == "Item" && card.traits != null && !card.traits.Exists((CardData.TraitStacks b) => b.data.name == "Consume"))
+                Debug.Log($"[Test] {card.title}");
+                if (card.cardType.name == "Item" && card.traits != null && !card.traits.Exists((CardData.TraitStacks b) => (b.data.name == "Consume" || b.data.name == "Recylce") ))
                 {
                     cardBase = card;
                     break;
@@ -58,13 +68,13 @@ namespace Pokefrost
             }
         }
 
+        public static IOrderedEnumerable<T> InPettyRandomOrder<T>(IEnumerable<T> source)
+        {
+            return source.OrderBy((T a) => Dead.PettyRandom.Range(0f, 1f));
+        }
+
         public IEnumerator UpdateData()
         {
-            for (int i = 0; i < 30; i++)
-            {
-
-                float r = Dead.PettyRandom.Range(0f, 1f);
-            }
             yield return RemoveData();
             CardData trueData = target.data;
             target.damage.current = cardBase.damage;
@@ -79,6 +89,7 @@ namespace Pokefrost
             trueData.damage = cardBase.damage;
             trueData.needsTarget = cardBase.needsTarget;
             trueData.playOnSlot = cardBase.playOnSlot;
+            trueData.titleFallback = cardBase.titleFallback;
             Card card = target.display as Card;
             card.SetName(cardBase.title);
 
@@ -97,5 +108,49 @@ namespace Pokefrost
             target.attackEffects = (from a in cardBase.attackEffects
                                     select a.Clone()).ToList();
         }
+    }
+
+    internal class StatusEffectGiveDreamCard : StatusEffectApplyX
+    {
+        public override void Init()
+        {
+            Events.OnRedrawBellHit += GiveCard;
+            base.OnCardMove += GiveCard2;
+            base.Init();
+        }
+
+        public override bool RunCardMoveEvent(Entity entity)
+        {
+            return (entity == target && WasMovedOnToBoard(entity));
+        }
+
+        private IEnumerator GiveCard2(Entity entity)
+        {
+            return Run(GetTargets());
+        }
+
+        private void OnDestroy()
+        {
+            Events.OnRedrawBellHit -= GiveCard;
+        }
+
+        private void GiveCard(RedrawBellSystem arg0)
+        {
+            if (Battle.IsOnBoard(target))
+            {
+                ActionQueue.Add(new ActionSequence(Run(GetTargets())), fixedPosition: true);
+            }
+        }
+
+        public static bool WasMovedOnToBoard(Entity entity)
+        {
+            if (Battle.IsOnBoard(entity))
+            {
+                return !Battle.IsOnBoard(entity.preContainers);
+            }
+
+            return false;
+        }
+
     }
 }
